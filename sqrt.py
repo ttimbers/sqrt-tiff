afa83e6559f38046c9baf59baa9ba1dde3b6435c
Tiffany Timbers <tiffany.timbers@gmail.com>
2024-11-18 10:28:09 -0800

    made a better error message

--- sqrt.py
+++ sqrt.py
@@ -9,6 +9,8 @@ import math
 @click.command()
 @click.option("--n", type=int, required=True, help="Number for which the square root should be calculated")
 def main(n):
+    if n < 0:
+        raise Exception("n should be positive")
     print(math.sqrt(n))
 
 if __name__ == "__main__":
